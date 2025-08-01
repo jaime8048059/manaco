904ff4bacc4f991c250922cc60b78fda67b36fd4
Jaime Clemente Ramirez <jaime.cklemente@uab.edu.bo>
2025-08-01 00:29:33 -0400

    completando

--- backend/app/Http/Controllers/CategoriaController.php
+++ backend/app/Http/Controllers/CategoriaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Model\Categoria;
+use App\Models\Categoria;
 
 class CategoriaController extends Controller
 {
--- backend/app/Http/Controllers/ClienteController.php
+++ backend/app/Http/Controllers/ClienteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Cliente;
 
 class ClienteController extends Controller
 {
@@ -14,6 +15,7 @@ class ClienteController extends Controller
     public function index()
     {
         //
+        return response()->json(Cliente::all());
     }
 
     /**
