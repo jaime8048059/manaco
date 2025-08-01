3bd1ac391659f9694466dffea540489b4faaf270
Jaime Clemente Ramirez <jaime.cklemente@uab.edu.bo>
2025-07-31 21:17:31 -0400

    modificacion

--- backend/app/Models/Categoria.php
+++ backend/app/Models/Categoria.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    use HasFactory;
+    protected $table='categoria';
+    protected $fillable=['nombre','descripcion'];
 }
