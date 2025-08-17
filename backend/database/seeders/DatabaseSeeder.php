1958c5a8a0409e7aef0fd05715af0a537f9e77f5
Jaime Clemente Ramirez <jaime.clemente@uab.bo.edu>
2025-08-17 19:34:29 -0400

    nuevo proyecto

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -9,18 +10,14 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run():void
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-         //User::factory()->create([
-           // 'name' => 'Test User',
-          // 'email' => 'test@example.com',
-      //  ]);
-        $this->call(CategoriaSeeder::class);
-        $this->call(ClienteSeeder::class);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
--- backend/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoriaController;
-
-
-Route::apiResource('categorias', CategoriaController::class);
