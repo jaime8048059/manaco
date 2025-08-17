e27f3c0c1ad030e87443d40e7f90fcfcfe7a4326
Jaime Clemente Ramirez <jaime.clemente@uab.bo.edu>
2025-08-17 15:44:20 -0400

    creacion categorias

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
 import { Home } from './home/home';
+import { Categorias } from './categorias/categorias';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,11 @@ export const routes: Routes = [
         path: '',
         component: Home,
         title: 'Inicio',
+      },
+      {
+        path: 'categorias',
+        component: Categorias,
+        title: 'categorias',
       },
     ],
   },
---
+++ frontend/src/app/categorias/categorias.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CategoriasService } from '../core/services/categorias.service';
+
+@Component({
+  selector: 'app-categorias',
+  imports: [],
+  templateUrl: './categorias.html',
+  styleUrl: './categorias.scss',
+})
+export class Categorias {
+  categorias: any[] = [];
+
+  constructor(private categoriaService: CategoriasService) {}
+  ngOninit(): void {
+    this.listarCategorias();
+  }
+  listarCategorias() {
+    this.categoriaService.listarCategorias().subscribe({
+      next: (rows) => {
+        this.categorias = rows;
+        console.log(this.categorias);
+      },
+    });
+  }
+}
